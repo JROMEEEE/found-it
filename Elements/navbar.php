6aedbd714d0fe71167c1f9cc4d73b85544ec666d
Romell <jromell099@gmail.com>
2025-03-15 09:04:44 +0800

    first commit

---
+++ Elements/navbar.php
@@ -0,0 +1,16 @@
+<nav class="shadow navbar custom-navbar sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="index.php">
+            <img src="assets/logo.png" width="45" height="45" class="d-inline-block align-middle me-2">
+            FOUND-IT!
+        </a>
+        <ul class="navbar-nav flex-row flex-wrap bd-navbar-nav">
+            <li class="nav-item col-6 col-lg-auto">
+                <a class="navbar-brand d-flex align-items-center" href="">Database</a>
+            </li>
+            <li class="nav-item col-6 col-lg-auto">
+                <a class="navbar-brand d-flex align-items-center" href="">Submit Lost Item</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include('Elements/navbar.php'); // NAVBAR ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP IMPORT -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>FOUND IT!</title>
+</head>
+<body>
+    
+<div class="container p-5 mt-3 mb-3">
+    <h1 class="text-center">Welcome to <span class="text-primary">FOUND-IT!</span></h1>
+    <h6 class="text-center">The Lipa Campus Lost & Found Services</h6>
+</div>
+
+
+
+
+<!-- BOOTSTRAP IMPORT -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
